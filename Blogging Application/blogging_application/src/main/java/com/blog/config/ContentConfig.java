41499bcc533132b0c0d81ac2995fcf3005168a20
rajshribhoyar <101972830+rajshribhoyar@users.noreply.github.com>
2022-12-13 14:55:31 +0530

    Add files via upload

---
+++ Blogging Application/blogging_application/src/main/java/com/blog/config/ContentConfig.java
@@ -0,0 +1,44 @@
+package com.blog.config;
+
+import java.awt.PageAttributes.MediaType;
+
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class ContentConfig implements WebMvcConfigurer{
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * @Override public void
+ * configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+ * 
+ * configurer.favorParameter(true) .parameterName("mediaType")
+ * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
+ * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML);
+ * 
+ * 
+ * }
+ */
